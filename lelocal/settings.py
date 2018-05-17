4cbfe620122ec4d0651f7b95942c04c23c5c0971
Jérôme Desmares <jdesmare@student.42.fr>
2018-05-17 17:32:33 +0200

    db problem

--- lelocal/settings.py
+++ lelocal/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -75,7 +77,7 @@ WSGI_APPLICATION = 'lelocal.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': ''
     }
 }
 
