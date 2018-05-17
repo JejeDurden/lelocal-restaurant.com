1c5d47f344d00924daeb65bd9364b09cc0f344b6
Jérôme Desmares <jdesmare@student.42.fr>
2018-05-17 15:45:39 +0200

    db problem

--- lelocal/settings.py
+++ lelocal/settings.py
@@ -131,3 +131,5 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
